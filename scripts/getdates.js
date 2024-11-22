accf301ba38ec9b8cb170c05a3fbf5c62f1ee633
freedombyui <131212585+freedombyu@users.noreply.github.com>
2024-11-22 10:23:03 +0200

    deleted javascript and css

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Dynamically set the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Dynamically set the last modified date
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
